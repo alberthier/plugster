90b7642255349375f7dc83a9c10d245edac3d65c
Eric ALBER <eric.alber@gmail.com>
2010-04-02 13:37:31 +0200

    Add a search filter in the elements factory view

--- plugster/lib/plugster/ElementFactoriesWidget.py
+++ plugster/lib/plugster/ElementFactoriesWidget.py
@@ -9,9 +9,13 @@ class ElementFactoriesWidget(gtk.VBox):
     def __init__(self):
         gtk.VBox.__init__(self)
 
+        self._filtered_paths = None
+        self._saved_expanded_paths = []
+
         self._entry = gtk.Entry()
         self._entry.connect("notify::text", self._on_filter_changed)
         self._entry.props.has_frame = False
+        self._entry.props.primary_icon_stock = gtk.STOCK_FIND
         self.pack_start(self._entry, False)
 
         self.pack_start(gtk.HSeparator(), False)
@@ -42,7 +46,9 @@ class ElementFactoriesWidget(gtk.VBox):
         self._treeview.set_tooltip_column(ElementFactoriesModel.TOOLTIP_COLUMN)
 
         self._treeview.enable_model_drag_source(gtk.gdk.BUTTON1_MASK, ElementFactoriesWidget.DRAG_TARGETS, gtk.gdk.ACTION_COPY)
-        self._treeview.set_model(ElementFactoriesModel())
+        self._model = gtk.TreeModelFilter(child_model = ElementFactoriesModel())
+        self._model.set_visible_func(self._is_visible)
+        self._treeview.set_model(self._model)
 
         self._treeview.connect('drag-data-get', self._drag_data_get_element_factory)
 
@@ -56,4 +62,44 @@ class ElementFactoriesWidget(gtk.VBox):
 
 
     def _on_filter_changed(self, object, pspec):
-        print self._entry.props.text
+        filter = self._entry.props.text
+        if len(filter) != 0:
+            if self._filtered_paths == None:
+                self._treeview.map_expanded_rows(lambda treeview, path: self._saved_expanded_paths.append(path))
+            self._filtered_paths = self._filter_children(None, filter.lower())
+        else:
+            self._filtered_paths = None
+        self._model.refilter()
+        if len(filter) == 0:
+            self._treeview.collapse_all()
+            for path in self._saved_expanded_paths:
+                self._treeview.expand_row(path, False)
+            self._saved_expanded_paths = []
+        else:
+            self._treeview.expand_all()
+
+
+    def _filter_children(self, current, filter):
+        filtered_paths = []
+        model = self._model.props.child_model
+        iter = model.iter_children(current)
+        if iter != None:
+            while iter != None:
+                filtered_paths += self._filter_children(iter, filter)
+                iter = model.iter_next(iter)
+            if len(filtered_paths) != 0:
+                if current != None:
+                    filtered_paths.append(model.get_path(current))
+        else:
+            (factory,) = model.get(current, ElementFactoriesModel.ELEMENT_FACTORY_COLUMN)
+            if filter in factory.get_longname().lower() or \
+               filter in factory.get_name().lower():
+                filtered_paths.append(model.get_path(current))
+        return filtered_paths
+
+
+    def _is_visible(self, model, iter):
+        if self._filtered_paths != None:
+            return model.get_path(iter) in self._filtered_paths
+        else:
+            return True
